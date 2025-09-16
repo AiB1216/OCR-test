599b91a9e13f50cf11c027d8bbf2ba43f93d4ca3
AiB1216 <162445880+AiB1216@users.noreply.github.com>
2025-09-16 08:17:04 +0000

    commit d1f5c3e4b6a7e8f9g0h1i2j3k4l5m6n7o8p9q0r

--- pad.py
+++ pad.py
@@ -1,3 +1,5 @@
-import paddlepaddle, pad
-
+from paddlex import create_pipeline
+pipeline = create_pipeline(pipeline='OCR', 
+                           paddlex_config='custom_ocr.yaml', 
+                           model_dir="./pad_model")
 print("ok")
